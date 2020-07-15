3a2486b4079bf5f9631e51110ded8edcc0de0785
punit2502 <punit.makwana25@gmail.com>
2020-07-15 18:28:47 +0530

    add flag address

--- src/components/Order/OrderTop/OrderTop.js
+++ src/components/Order/OrderTop/OrderTop.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import Bookmark from './Bookmark/Bookmark';
+import axios from 'axios';
 import { I18n } from 'react-i18next';
+import Bookmark from './Bookmark/Bookmark';
 import styles from './OrderTop.scss';
+import config from 'Config';
 
 class OrderTop extends Component {
   constructor(props) {
@@ -10,6 +12,21 @@ class OrderTop extends Component {
     this.state = {
       showBookmarkModal: false,
     };
+    this.flagOrder = this.flagOrder.bind(this);
+  }
+
+  flagOrder() {
+    axios.patch(
+      `${config.API_BASE_URL}/orders/${this.props.order.unique_reference}`,
+      {
+        flagged: true,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.token}`,
+        },
+      }
+    );
   }
 
   render() {
@@ -17,10 +34,15 @@ class OrderTop extends Component {
       <I18n ns="translations">
         {t => (
           <div>
-            <div className="col-xs-12">
-              <h3 className={styles.ref}>
-                {t('order.reference')}: <b>{this.props.order.unique_reference}</b>
-              </h3>
+            <div className={`col-xs-12 ${styles.container}`}>
+              <div className={styles.left}>
+                <h3 className={styles.ref}>
+                  {t('order.reference')}: <b>{this.props.order.unique_reference}</b>
+                </h3>
+                <button className={styles.flagOrder} onClick={this.flagOrder}>
+                  Flag Order
+                </button>
+              </div>
 
               <button
                 type="button"
